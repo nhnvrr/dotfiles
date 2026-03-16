5f7a1ac894a62c112f0b6b2d04e4d2505b33e569
Nicolas Navarro <navarropaeznicolas@gmail.com>
2026-03-17 00:25:08 +0100

    Update

--- nvim/lua/nhnvrr/plugins/colorscheme.lua
+++ nvim/lua/nhnvrr/plugins/colorscheme.lua
@@ -1,74 +1,22 @@
 return {
   {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
+    "nhnvrr/nordaurora.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      local function use_dark_theme()
-        if vim.fn.has("macunix") == 1 then
-          local result = vim.fn.system("defaults read -g AppleInterfaceStyle 2>/dev/null")
-          return vim.v.shell_error == 0 and result:match("Dark") ~= nil
-        end
-
-        return vim.o.background == "dark"
-      end
-
-      local use_dark = use_dark_theme()
-      local github_variant = use_dark and "github_dark" or "github_light"
-      vim.o.background = use_dark and "dark" or "light"
-
-      require("github-theme").setup({
-        options = {
-          compile_path = vim.fn.stdpath("cache") .. "/github-theme",
-          compile_file_suffix = "_compiled",
-          hide_end_of_buffer = true,
-          hide_nc_statusline = true,
-          transparent = true,
-          terminal_colors = true,
-          dim_inactive = false,
-          module_default = true,
-          styles = {
-            comments = "italic",
-            functions = "NONE",
-            keywords = "NONE",
-            variables = "NONE",
-            conditionals = "NONE",
-            constants = "NONE",
-            numbers = "NONE",
-            operators = "NONE",
-            strings = "NONE",
-            types = "NONE",
-          },
-          inverse = {
-            match_paren = false,
-            visual = false,
-            search = false,
-          },
-          darken = {
-            floats = true,
-            sidebars = {
-              enable = true,
-              list = {},
-            },
-          },
-          modules = {},
-        },
-        palettes = {},
-        specs = {},
-        groups = {},
-      })
-      vim.cmd.colorscheme(github_variant)
+    opts = {
+      transparent = false,
+      dim_inactive = false,
+      colors = {
+        blue = "#81A1C1",
+      },
+      on_highlights = function(hl, c)
+        hl.CursorLineNr = { fg = c.yellow, bold = true }
+        hl.Comment = { fg = "#6c7a92", italic = true }
+      end,
+    },
+    config = function(_, opts)
+      require("nordaurora").setup(opts)
+      vim.cmd.colorscheme("nordaurora")
     end,
-  },
-  {
-    "wtfox/jellybeans.nvim",
-    name = "jellybeans",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("jellybeans").setup({})
-      -- vim.cmd("colorscheme jellybeans")
-    end,
-  },
+  }
 }
